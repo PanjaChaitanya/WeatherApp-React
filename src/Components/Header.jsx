ab11dccb045c92a27716dc820cabca171a2532ae
PanjaChaitanya <panjachaitanya23@gmail.com>
2025-04-07 11:57:12 +0530

    updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,45 @@
 import React from 'react'
+import { useState, useEffect} from 'react'
 import Weather from './Weather'
 const Header = () => {
+
+  const [weatherData, setWeatherData] = useState([])
+  let api_key = '76a427aa28f8c63c16dde43bd96a8fcd';
+
+  const currentLocationWeather = () =>{
+
+    let getCurrentWeather = async(positionData)=>{
+       let lat = positionData.coords.latitude;
+       let lon = positionData.coords.longitude;
+       try{
+         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`)
+        .then(response => response.json())
+        .then((data)=>{
+          setWeatherData(data)
+        }).catch((error)=>{
+          console.log(error)
+        })
+
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`)
+        .then(response => response.json())
+        .then((data)=>{
+          console.log(data)
+        }).catch((error)=>{
+          console.log(error)
+        })
+      }catch(error){
+        console.log(error);
+      }
+    }
+    let currentPositionError = async(error)=>{
+       console.log(error)
+    }
+    //navigator geolocation getacurrentposition is pre defined js object to get users location. It takes two functions as params  first one is executed when succes and it returns an object , second fun is executed when there is an error
+    navigator.geolocation.getCurrentPosition(getCurrentWeather, currentPositionError);
+  }
+  useEffect(() => {
+    console.log("Updated weatherData:", weatherData);
+  }, [weatherData]);
   return (
     <>
         <header className='flex flex-wrap justify-between p-5 border-b-2 sticky top-0 border-b-gray-300 bg-white'>
@@ -15,11 +54,12 @@ const Header = () => {
                   type="text"
                   id="name"
                   placeholder="Enter Location"
-                  class="bg-gray-100 border-b-2 border-gray-300 text-indigo-900 px-3 focus:outline-none focus:border-sky-300"
+                  className="bg-gray-100 border-b-2 border-gray-300 text-indigo-900 px-3 focus:outline-none focus:border-sky-300"
               />
               <button className='bg-sky-300 cursor-pointer rounded-full p-1 hover:shadow-black hover:shadow-xs'><img src="images/search.svg" className='max-w-8' alt="" /></button>
             </div>
-            <div className='bg-sky-300 cursor-pointer p-2 rounded-2xl font-bold flex align-middle max-h-10'>
+            <div className='bg-sky-300 cursor-pointer p-2 rounded-2xl font-bold flex align-middle max-h-10'
+            onClick={currentLocationWeather}>
               <span className='flex'>
                 <img src="images/target.png" className='' alt="" />
               </span>
@@ -27,7 +67,7 @@ const Header = () => {
             </div>
           </section>
         </header>
-        <Weather/>
+        <Weather weatherData ={weatherData}/>
     </>
   )
 }
--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,63 +1,93 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
-const Weather = () => {
+const Weather = ({weatherData}) => {
+    
+    
+    if (!weatherData.main) {
+        return <div className='p-5 text-center text-lg'>Click on "Current Location" to fetch weather data.</div>;
+    }
+    const [presentDate, setPresentDate] = useState(null)
+    const [sunRiseTime, setSunRiseTime] = useState('')
+    const [sunSetTime, setSunSetTime] = useState('')
+    
+
+    const SunRiseSetTime =(timestamp)=>{
+
+        const date = new Date(timestamp * 1000);
+        let hours = date.getHours();
+        let mins = date.getMinutes();
+        
+        hours = hours % 12; 
+        hours = hours < 10 ? '0'+ hours : hours
+        hours = hours ? hours : 12; // The hour '0' should be '12'
+        mins = mins < 10 ? '0' + mins : mins;  // Add leading zero to minutes
+        return `${hours}:${mins}`;
+    }
+    useEffect(() => {
+        let date = new Date()
+        setPresentDate(date.toDateString())
+        setSunRiseTime(SunRiseSetTime(weatherData.sys.sunrise))
+        setSunSetTime(SunRiseSetTime(weatherData.sys.sunset))
+    }, [])
+
   return (
     <>
-    <main className='weather-main m-3 flex gap-5 min-h-screen'>
-        <aside className='flex-1/4'>
+    <main className='weather-main m-3 flex flex-wrap gap-2 min-h-screen'>
+        <aside className=''>
             <section className='current-weather'>
                 <div className='weather-card shadow-xl border-1 border-gray-100 p-2'>
                     <div className='date-location border-b-1 border-gray-400'>
                         <div className="location flex items-center gap-2">
                             <img src="icons/location.png" className='max-w-5' alt="" />
-                            <span className='text-gray-500 font-semibold'>Bhimavaram</span>
+                            <span className='text-gray-500 font-semibold'>{weatherData.name}, {weatherData.sys.country}</span>
                         </div>
                         <div className="date flex items-center gap-2">
                             <img src="icons/date.png" className='max-w-5' alt="" />
-                            <span className='text-gray-500 font-semibold'>Wednesday 12, April</span>
+                            <span className='text-gray-500 font-semibold'>{presentDate}</span>
                         </div>
                     </div>
                     <h2 className='font-bold text-sky-300'>Now</h2>
                     <div className='flex justify-between items-center'>
                         <div className='flex'>
                             <img src="icons/thermometer.png" className='max-w-8' alt="" />
-                            <h1 className='text-5xl font-bold'>35&deg;c</h1>
+                            <h1 className='text-5xl font-bold'>{weatherData.main.temp}&deg;c</h1>
                         </div>
                         <img src="images/7.png" alt="cloud-image" />
                     </div>
                     <div className='card-footer flex justify-between'>
-                        Feels like 37&deg;c 
-                        <p>Broken Clouds</p>
+                       <p> Feels like {weatherData.main.feels_like}&deg;c </p>
+                        <p> {weatherData.weather[0].description} </p>
                     </div>
                 </div>
             </section>
             <section className='weather-highlights'>
-                <h2 className='text-2xl font-bold'>Highlights</h2>
-                <div className="highlights-card">
-                    <div className="air-quality flex justify-between">
+                <h2 className='text-2xl font-bold mt-2 mb-1'>Highlights</h2>
+                <div className="highlights-card shadow-xl p-2 border-1 border-sky-100 rounded-xl">
+                    {/* <div className="air-quality flex justify-between">
                         <p>Air quality</p>
                         <button className=''>Good</button>
-                    </div>
+                    </div> */}
                     <div className="sun flex justify-between">
                         <div className="sun-rise flex items-center">
                             <img src="images/sunrise.png" alt="" />
                             <div className=''>
                                 <h4>Sunrise</h4>
-                                <p className='sun-time text-xl font-semibold'>05:46 AM</p>
+                                <p className='sun-time text-xl font-semibold'>{sunRiseTime} AM</p>
                             </div>
                         </div>
                         <div className="sun-set flex items-center">
                         <img src="images/sunset.png" alt="" />
                             <div>
                                 <h4>Sunset</h4>
-                                <p className='sun-time text-xl font-semibold'>06:35 PM</p>
+                                <p className='sun-time text-xl font-semibold'>{sunSetTime} PM</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
         </aside>
-        <article className='flex-3/4 border-2 border-blue-500'>
+        <article className='border-2 border-blue-500'>
             <section className='hourly-forecast'></section>
             <section className='upcoming-forecast'></section>
         </article>
